b54753a8081968573e97d89c7b389d8c60b3eff6
NavinKumar-nk <130886610+NavinKumar-nk@users.noreply.github.com>
2023-04-17 21:35:49 +0530

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+text = "tesr"
+
+rev = ""
+
+i = len(text)-1
+while (i >= 0):
+    rev += text[i]
+    i -= 1
+
+
+if text == rev:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+
+
+
+
+
+
+#while i=
+# rev1=rev+text
+#if(rev1==text):
+   # print("Plaindrome")
+#else:
+   # print("not Plaindrome")
+
+
+
---
+++ factorial.py
@@ -0,0 +1,6 @@
+num =1
+fact=5
+
+for i in range(1,fact+1):
+    num=num*i
+    print(num)
